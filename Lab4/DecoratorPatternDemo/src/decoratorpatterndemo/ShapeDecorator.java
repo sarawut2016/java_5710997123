0a666e3f49bb4a3849376b496be39d159ae03a67
sarawut2016 <sarawut.moks57@sskru.ac.th>
2017-02-18 11:35:00 +0700

    Lab4

---
+++ Lab4/DecoratorPatternDemo/src/decoratorpatterndemo/ShapeDecorator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decoratorpatterndemo;
+
+/**
+ *
+ * @author Kade
+ */
+public class ShapeDecorator  implements Shape {
+    public  ShapeDecorator(){
+    }
+    public void draw(){
+    }
+    
+}

f5db39e188f7be046e02732551c263165c874be5
sarawut2016 <sarawut.moks57@sskru.ac.th>
2017-03-06 15:39:12 +0700

    Lab5

---
+++ Lab4/Lad5/src/Org/sskru/domain/User.java
@@ -0,0 +1,18 @@
+package Org.sskru.domain;
+
+public class User {
+
+    private Object name;
+
+    public void User() {
+    }
+
+    public void setName() {
+    }
+
+    public void getName() {
+    }
+
+    public void sendMessage() {
+    }
+}
